a7e4cd4ee2a7e882b93291839dbce7f0ed012a0b
alerafart <maalejandrarafart@gmail.com>
2022-05-11 22:07:46 +0200

    fix no changes just previous git pull

--- app/Models/Developers.php
+++ app/Models/Developers.php
@@ -9,7 +9,6 @@ class developers extends Model
 {
     use hasFactory;
 
-<<<<<<< HEAD
     /**
      * defining DB relationships
      *
@@ -24,10 +23,4 @@ class developers extends Model
     }
 
 
-=======
-    public function users()
-    {
-        return $this->hasOne(Users::class);
-    }
->>>>>>> 8623d7342f05b94ab4acdef34669ede0f715720c
 }
--- app/Models/Languages.php
+++ app/Models/Languages.php
@@ -9,7 +9,6 @@ class Languages extends Model
 {
     use HasFactory;
 
-<<<<<<< HEAD
      /**
      * defining DB relationships
      *
@@ -17,10 +16,6 @@ class Languages extends Model
      */
     public function developers() {
         return $this->HasMany('App\Models\Developers', 'dev_lang');
-=======
-    public function developers() {
-        return $this->HasMany('App\Models\Developers');
->>>>>>> 8623d7342f05b94ab4acdef34669ede0f715720c
     }
 
 }
