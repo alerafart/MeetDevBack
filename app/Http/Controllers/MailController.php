f7dc2c9161ef3e351e8a2a9e2bc98cfcd388b07c
AliciaMV <milekic.alicia@gmail.com>
2022-05-14 20:21:30 +0200

    test(MailController)

--- app/Http/Controllers/MailController.php
+++ app/Http/Controllers/MailController.php
@@ -16,17 +16,17 @@ class MailController extends Controller
         /*$name = 'Cloudways';
         Mail::to('Cloudways@Cloudways.com')->send(new SendMailable($name));*/
 
-        $to_name = 'RECEIVER_NAME';
-        $to_email = 'RECEIVER_EMAIL_ADDRESS';
+        $to_name = 'georges';
+        $to_email = 'milekic.alicia@gmail.com';
         $data = array('name'=>"Cloudways (sender_name)", "body" => "A test mail");
 
         Mail::send('mail', $data, function($message) use ($to_name, $to_email) {
         $message->to($to_email, $to_name)
         ->subject('Laravel Test Mail');
-        $message->from('SENDER_EMAIL_ADDRESS','Test Mail');
+        $message->from('meetdev.apollo22@gmail.com','Test Mail');
         });
 
-        return response()->json(['status' => 'success', 'message' =>  'Email sent Successfully']);
+        return response()->json(['status' => 'success', 'message' =>  'Email sent Successfully', 'data' => $data]);
     }
 
 
