4c3a178ffc283c618063ba0cf61b51c2b3f48ef5
AliciaMV <milekic.alicia@gmail.com>
2022-05-19 19:23:24 +0200

    feature(routes) -prepare all routes for jwt auth implementation

--- app/Models/Developers.php
+++ app/Models/Developers.php
@@ -22,4 +22,23 @@ class developers extends Model
         return $this->HasMany('App\Models\Languages', 'dev_lang');
     }
 
+
+    /**
+     * The model's default values for attributes.
+     *
+     * @var array
+     */
+    protected $attributes = [
+        'available_for_recruiters' => 1,
+        'available_for_developers' => 0,
+    ];
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'lastname', 'firstname', 'city', 'department', 'zip_code', 'email_address', 'phone', 'password', 'subscribe_to_push_notif', 'profile_picture'
+    ];
 }
