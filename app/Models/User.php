d82b0d0b4eee41e56a159b3caec25d2928a342ee
AliciaMV <milekic.alicia@gmail.com>
2022-05-30 14:16:05 +0200

    test on server

--- app/Models/User.php
+++ app/Models/User.php
@@ -14,7 +14,7 @@ use Illuminate\Foundation\Auth\User as Authenticatable;
 
 class User extends Authenticatable implements AuthenticatableContract, AuthorizableContract, JWTSubject
 {
-    use Notifiable, Authenticatable, MustVerifyEmail;
+    use Authorizable, Notifiable, MustVerifyEmail;
 
      /**
      * Get the identifier that will be stored in the subject claim of the JWT.
